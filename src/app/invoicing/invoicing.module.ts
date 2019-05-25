e1a0638f6f563152a56463839308bc8442078449
maksg <maksg@onet.eu>
2019-05-25 16:52:40 +0200

    Download autocomplete list from Google

--- src/app/invoicing/invoicing.module.ts
+++ src/app/invoicing/invoicing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InvoiceComponent } from './invoice/invoice.component';
 import { SinglePositionComponent } from './single-position/single-position.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { ItemCatalog } from './model/item-catalog/item-catalog';
 import { LocalItemCatalog } from './model/item-catalog/local-item-catalog';
+import { HttpItemCatalog } from './model/item-catalog/http-item-catalog';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
@@ -18,8 +19,11 @@ import { FormsModule } from '@angular/forms';
     InvoiceComponent
   ],
   providers: [
+    // {
+    //   provide: ItemCatalog, useFactory: () => new LocalItemCatalog()
+    // }
     {
-      provide: ItemCatalog, useFactory: () => new LocalItemCatalog()
+      provide: ItemCatalog, useFactory: (http: HttpClient) => new HttpItemCatalog(http), deps: [HttpClient]
     }
   ]
 })
